859369ae17e5d94527f99c98c6fac6ea7f8bf0a0
Juanse616 <69650831+Juanse617@users.noreply.github.com>
2023-07-16 23:49:53 -0500

    india ayuda

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import app from "./app/app.js";
 
+import http from http;
 
-app.listen(app.get("port"), ()=>{
-    console.log(`estas en puerto: http://localhost:${app.get("port")}`);
+const hostname = '0.0.0.0';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end ('Zeet Node');
+});
+
+app.listen(app.get("port", hostname), ()=>{
+    console.log(`estas en puerto: http://${hostname}:${app.get("port")}`);
 })
